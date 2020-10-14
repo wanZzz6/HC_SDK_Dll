f56a850ff9ccf08bfc1da5d129b73c8b3bc0301b
wanzheng <1343837706@qq.com>
2020-10-14 14:05:22 +0800

    🌱测试buf

--- test/test_buf.py
+++ test/test_buf.py
@@ -2,15 +2,9 @@ import Struct
 import ctypes
 
 a = b'\xe4\x07\x00\x00\t\x00\x00\x00\x11\x00\x00\x00\x0f\x00\x00\x00\x15\x00\x00\x008\x00\x00\x00'
+print(list(a))
 
 b = Struct.NET_DVR_TIME.from_buffer_copy(a)
-print(ctypes.sizeof(b), len(a))
-print(list(a))
-print(b.dwYear)
-print(b.dwMonth)
-print(b.dwDay)
-print(b.dwHour)
-print(b.dwMinute)
-print(b.dwSecond)
+print('结构体大小：', ctypes.sizeof(b), '缓存块大小：', len(a))
 
-
+print('结构体成员：{}-{}-{} {}:{}:{}'.format(b.dwYear, b.dwMonth, b.dwDay, b.dwHour, b.dwMinute, b.dwSecond))
